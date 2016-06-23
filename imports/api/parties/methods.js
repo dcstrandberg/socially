1bc90e86344066e600d185c33699293f1ca54313
David Mac <dcstrandberg@gmail.com>
2016-06-23 15:25:47 -0500

    Added inviting methods (and methods in general) as well as email functionality.
    Ran:
    
    meteor add check
    
    and
    
    meteor add email
    
    In order to check Meteor Method parameters and to send emails.

---
+++ imports/api/parties/methods.js
@@ -0,0 +1,70 @@
+import _ from 'underscore';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Email } from 'meteor/email';
+
+import { Parties } from './collection';
+
+function getContactEmail(user) {
+    if (user.emails && user.emails.length)
+        return user.emails[0].address;
+    
+    if (user.services && user.services.facebook && user.services.facebook.email)
+        return user.services.facebook.email;
+
+    return null;
+}
+
+export function invite(partyId, userId) {
+    check(partyId, String);
+    check(userId, String);
+
+    if(!this.userId) {
+        throw new Meteor.Error(400, 'You have to be signed in!');
+    }
+
+    const party = Parties.findOne(partyId);
+
+    if (!party) {
+        throw new Meteor.Error(404, 'No such party exists');
+    }
+
+    if (party.owner !== this.userId) {
+        throw new Meteor.Error(404, 'No permissions!');
+    }
+
+    if (party.public) {
+        throw new Meteor.Error(400, 'That party is public. No need to invite people');
+    }
+
+    if (userId !== party.owner && ! _.contains(party.invited, userId)) {
+        Parties.update(partyId, {
+            $addToSet: {
+                invited: userId
+            }
+        });
+
+        const replyTo = getContactEmail(Meteor.users.findOne(this.userId));
+        const to = getContactEmail(Meteor.users.findOne(userId));
+
+        if (Meteor.isServer && to) {
+            Email.send({
+                to,
+                replyTo,
+                from: 'noreply@socially.com',
+                subject: `Party: ${party.name}`,
+                text: `
+                    Hey, I just invited you to ${party.name} on Socially.
+
+                    '${party.description}'
+
+                    Come check it out: ${Meteor.absoluteUrl()}
+                    `
+            });
+        }
+    }
+}
+
+Meteor.methods({
+    invite
+});
